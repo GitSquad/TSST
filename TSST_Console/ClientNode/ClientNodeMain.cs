b529dd56015db4099da7482f0dcdf51a01e5073f
tosinski <9tomek3@gmail.com>
2014-11-19 18:25:27 +0100

    TSST_projekt
    łapcie projekt z tsst

---
+++ TSST_Console/ClientNode/ClientNodeMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientNode
+{
+    class ClientNodeMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wezeł kliencki odpalony!");
+            Console.WriteLine("Wybierz opcje:");
+            Console.WriteLine("1. Przeslij wiadomość");
+            Console.WriteLine("2. Pokaz odebrane wiadomości");
+            Console.WriteLine("3. Wyjdź z programu");
+
+            int choice;
+            choice = int.Parse(Console.ReadLine());
+            
+            switch(choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Podaj wiadomość do przesłania");
+                        Console.ReadLine();
+
+                        break;
+                    }
+                case 2:
+                    {
+                        break;
+                    }
+                case 3:
+                    {
+                        break;
+                    }
+
+            }
+
+        }
+    }
+}
---
+++ TSST_Console/NetworkManager/ManagerMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkManager
+{
+    class ManagerMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odpalilem managera");
+            Console.ReadLine();
+        }
+    }
+}
