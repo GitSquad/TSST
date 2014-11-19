e86c7e3da599cbb63e474cbdae11e94f04e854de
tosinski <9tomek3@gmail.com>
2014-11-19 23:45:42 +0100

    zmiany
    mozna wysylac wiadomosc z client node do Cloud

--- TSST/ClientNode/ClientNodeMain.cs
+++ TSST/ClientNode/ClientNodeMain.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,38 +10,66 @@ namespace ClientNode
 {
     class ClientNodeMain
     {
+        private static Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         static void Main(string[] args)
         {
-            Console.WriteLine("Wezeł kliencki odpalony!");
-            Console.WriteLine("Wybierz opcje:");
-            Console.WriteLine("1. Przeslij wiadomość");
-            Console.WriteLine("2. Pokaz odebrane wiadomości");
-            Console.WriteLine("3. Wyjdź z programu");
+                Boolean q = true;
+                Console.WriteLine("Wezeł kliencki odpalony!");
+                Connect();
+                while(q)
+                {
+                
+                Console.WriteLine("Wybierz opcje:");
+                Console.WriteLine("1. Przeslij wiadomość");
+                Console.WriteLine("2. Pokaz odebrane wiadomości");
+                Console.WriteLine("3. Wyjdź z programu");
 
-            int choice;
-            choice = int.Parse(Console.ReadLine());
-            
-            switch(choice)
+                int choice;
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        {
+
+                            Console.WriteLine("Podaj wiadomość do przesłania");
+                            String message = Console.ReadLine();
+                            Console.WriteLine(message);
+                            byte[] data = Encoding.ASCII.GetBytes(message);
+                            clientSocket.Send(data);
+                            Console.WriteLine("Wyslano: " + message);
+
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+
+                            clientSocket.Close();
+                            q = false;
+                            break;
+                        }
+                }
+                }
+                Console.WriteLine("Koniec");
+            Console.Read();
+        }
+
+        private static void Connect()
+        {
+            try
             {
-                case 1:
-                    {
-                        Console.WriteLine("Podaj wiadomość do przesłania");
-                        String message = Console.ReadLine();
-                        Console.WriteLine(message);
-                        
-                        break;
-                    }
-                case 2:
-                    {
-                        break;
-                    }
-                case 3:
-                    {
-                        break;
-                    }
+                clientSocket.Connect(IPAddress.Loopback, 1020);
+            }
+            catch
+            {
 
             }
-            Console.Read();
+            Console.WriteLine("Connected");
         }
     }
 }
--- TSST/Cloud/CloudMain.cs
+++ TSST/Cloud/CloudMain.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +10,56 @@ namespace Cloud
 {
     class CloudMain
     {
+        static byte[] Buffer = new byte[1024];
+
+        private static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         static void Main(string[] args)
         {
             Console.WriteLine("Chmura odpalona");
+            startServer();
+            
+        }
+
+        private static void startServer()
+        {
+            Console.WriteLine("Starting the server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 1020));
+            serverSocket.Listen(100);
+            //serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+            Socket accepted = serverSocket.Accept();
+            Console.WriteLine("Server is ON");
+            accepted.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), accepted);
+            
+            
+            
+            //serverSocket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), accepted);
+            
+           
+   
             Console.Read();
+            
+           
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = serverSocket.EndAccept(AR);
+            Console.WriteLine("Server is ON");
+            socket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+        }
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] data = new byte[received];
+            Array.Copy(Buffer, data, received);
+            String strData = Encoding.ASCII.GetString(data);
+            
+            Console.WriteLine("Otrzymalem: " + strData);
+            socket.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
         }
     }
 }
